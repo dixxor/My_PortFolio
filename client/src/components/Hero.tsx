67c2c53df014cff7fe3a12a7a7790439f95c9fb2
Dinoda rathnayake <10900371@students.plymouth.ac.uk>
2025-06-04 19:31:13 +0530

    Update Hero.tsx

--- client/src/components/Hero.tsx
+++ client/src/components/Hero.tsx
@@ -54,7 +54,7 @@ export default function Hero() {
           className="mb-6 mt-6"
         >
           <Avatar className="w-56 h-56 mx-auto shadow-2xl border-4 border-white/20">
-            <AvatarImage src="https://i.ibb.co/9m2HxTF8/IMG-20250528-WA0030.jpg" alt="Dinoda Rathnayake photo" className="object-cover" />
+            <AvatarImage src="https://cdn.pixabay.com/photo/2023/12/06/21/07/photo-8434386_1280.jpg" alt="Dinoda Rathnayake photo" className="object-cover" />
             <AvatarFallback>DR</AvatarFallback>
           </Avatar>
         </motion.div>
